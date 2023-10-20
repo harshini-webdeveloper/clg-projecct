d4ab3e2522280f5768fecad6340ca3ec05025fae
harshini-webdeveloper <148410205+harshini-webdeveloper@users.noreply.github.com>
2023-10-20 12:07:13 +0000

    database is connected

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, jsonify, render_template, request
+
+from database import load_job_from_db, load_jobs_from_db, add_application_to_db
 
 app = Flask(__name__)
+
 
 
 @app.route("/")
@@ -29,6 +31,20 @@ def show_job(id):
   if not job:
     return "Job Not Found", 404
   return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply",
+           methods=['post'])  #APPLY THA JOB ID VARA THA APPLY THA JOB ID
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  # Store this in the DB
+  # Store an email
+  # Display an acknowledgment page
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 
 print(__name__)
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from typing import Any
 from sqlalchemy import create_engine, text
+from collections import namedtuple
 from sqlalchemy.sql.schema import Sequence
 import os
 
@@ -29,3 +30,39 @@ def load_job_from_db(id):
       return None
     else:
       return rows[0]._asdict()
+
+
+#def add_application_to_db(job_id, data):
+# with engine.connect() as conn:
+#  query = text(
+#     "INSERT INTO application (job_id, fully_name, email, phone, education)  VALUES #(:job_id, :fully_name, :email, :phone, :education) "
+#)
+
+
+#conn.execute(
+#   query,
+#  {
+#     job_id: job_id,
+# "fully_name": data['fully_name'],
+#    "email": data['email'],
+#   "phone": data['phone'],
+#  "education": data['education']
+# })
+def add_application_to_db(job_id, data):
+  ApplicationData = namedtuple(
+      'ApplicationData',
+      ['job_id', 'fully_name', 'email', 'phone', 'education'])
+
+  # Create an instance of the named tuple
+  application_data = ApplicationData(job_id=job_id,
+                                     fully_name=data.get('fully_name'),
+                                     email=data['email'],
+                                     phone=data['phone'],
+                                     education=data['education'])
+
+  with engine.connect() as conn:
+    query = text(
+        "INSERT INTO application (job_id, fully_name, email, phone, education) "
+        "VALUES (:job_id, :fully_name, :email, :phone, :education)")
+
+    conn.execute(query, application_data._asdict())
